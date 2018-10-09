c75da5023c486f07c0a676add789389040069b24
es <es@evsfy.com>
2018-10-09 19:19:08 +0800

    updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,53 +1,53 @@
-const gulp = require('gulp'),
-      hercule = require('hercule'),
-      through = require('through2'),
-      drakov = require('drakov'),
-      aglio = require('gulp-aglio'),
-      plumber = require('gulp-plumber'),
-      browserSync = require('browser-sync').create()
+const gulp = require('gulp')
+const hercule = require('hercule')
+const through = require('through2')
+const drakov = require('drakov')
+const plumber = require('gulp-plumber')
+const browserSync = require('browser-sync').create()
+const path = require('path')
+const aglio = require('gulp-aglio')
 
 var filename = './src/api.md'
 var targetfile = './build/api.md'
 
-gulp.task('hercule', function(cb) {
-  function gulpHercule() {
-    return through.obj(function(file, encoding, callback) {
+gulp.task('hercule', function (cb) {
+  function gulpHercule () {
+    return through.obj(function (file, encoding, callback) {
       if (file.isNull()) {
-        throw new Error ('File is empty.')
+        throw new Error('File is empty.')
       }
-
       if (file.isStream()) {
-        var transcluder = new hercule.TranscludeStream();
-        file.contents = file.contents.pipe(transcluder);
-        return callback(null, file);
+        let srcPath = path.resolve(process.cwd(), './src/')
+        var transcluder = new hercule.TranscludeStream(srcPath)
+        file.contents = file.contents.pipe(transcluder)
+        return callback(null, file)
       }
-    });
+    })
   }
   gulp.src(filename, { buffer: false })
     .pipe(gulpHercule())
     .pipe(gulp.dest('./build/'))
     .on('end', cb)
-});
+})
 
 gulp.task('docs', ['hercule'], function (cb) {
   gulp.src(targetfile)
   .pipe(plumber())
-  .pipe(aglio({
-    themeTemplate: '/Users/es/Tools/aglio-template/index.jade'
-  }))
+  .pipe(aglio({ template: 'default' }))
   .pipe(gulp.dest('./docs/'))
   .on('end', cb)
 })
 
 gulp.task('docserver', ['docs'], function () {
   browserSync.init({
+    port: 3000,
     server: {
       baseDir: 'docs',
       index: 'api.html'
     },
     files: ['./docs'],
     ui: {
-      port: 3000
+      port: 3001
     },
     open: false
   })
@@ -56,10 +56,10 @@ gulp.task('docserver', ['docs'], function () {
 gulp.task('drakov', ['hercule'], function () {
   const argv = {
     sourceFiles: './build/api.md',
-    serverPort: 8000,
+    serverPort: 7000,
     header: 'Authorization',
     autoOptions: true,
-    method: 'PUT'
+    method: 'PUT, DELETE, PATCH'
   }
   drakov.stop()
   drakov.run(argv)
@@ -68,7 +68,9 @@ gulp.task('drakov', ['hercule'], function () {
 const tasks = ['hercule', 'drakov', 'docs']
 
 gulp.task('build', tasks)
-gulp.task('watch', function() {
-  gulp.watch(['./src/*.md'], ['build']);
+gulp.task('watch', function () {
+  gulp.watch(['./src/**/*.md'], ['hercule', 'docs', 'drakov'])
 })
+gulp.task('page', ['hercule', 'docs'])
+gulp.task('mock', ['hercule', 'drakov'])
 gulp.task('default', ['hercule', 'drakov', 'docs', 'docserver', 'watch'])
